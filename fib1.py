ee906eb2aef383b55c113d5063035d225a9a0025
bhuveshgautam <42464227+bhuveshgautam@users.noreply.github.com>
2018-10-08 18:32:41 +0530

    Update fib1.py

--- fib1.py
+++ fib1.py
@@ -1,10 +1,11 @@
-def fib (n):
-	if n < 2:
-		return n
-	return fib(n-3)+fib(n-1)
+def fib(n):
+	if(n < 2):
+		return(n)
+	else:
+		return(fib(n-3)+fib(n-1))
 
 
-n = input("Enter the number of times you want to input n :")
+n = int(input("Enter the number of times you want to input n :"))
 for i in range (0,n):
-	a = input ("Enter number :")
+	a = input("Enter number :")
 	print fib(a)
